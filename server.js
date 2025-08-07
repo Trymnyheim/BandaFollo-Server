b92eee35de35cceba21fd7b84e2771e23a822afa
trymhnyheim <trym.haakon.nyheim@gmail.com>
2025-08-07 23:33:21 +0200

    increase send limit

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 app.get('/', (_req, res) => {
     res.send('<p>Server for Banda Follo\'s website. <a href="https://www.bandafollo.no">Click here<a> to visit.<p>')
